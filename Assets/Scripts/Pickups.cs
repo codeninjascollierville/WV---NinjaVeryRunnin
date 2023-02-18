048e99f8ab5cfd2594aee30c4b5f4c948688af6b
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-02-17 18:43:29 -0600

    Initial commit

---
+++ Assets/Scripts/Pickups.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickups : MonoBehaviour
+{
+    public ParticleSystem Pickup;
+    void Start()
+    {
+       Pickup.Stop();
+    }
+
+    void Update()
+    {
+    
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("coin"))
+        {
+            Destroy(other.gameObject);
+
+            Pickup.Play();
+
+        }
+    }
+    void DestroyDoor(gameObject other)
+    {
+        
+    }
+}
